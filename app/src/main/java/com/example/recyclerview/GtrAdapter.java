9597408e45449faaad0d7f98d8d2fa94f2351686
randomize721 <anggadp91@gmail.com>
2019-04-14 19:35:00 +0800

    Working onClick

--- app/src/main/java/com/example/recyclerview/GtrAdapter.java
+++ app/src/main/java/com/example/recyclerview/GtrAdapter.java
@@ -1,15 +1,21 @@
 package com.example.recyclerview;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 public class GtrAdapter extends RecyclerView.Adapter<GtrAdapter.GtrViewHolder> {
 
@@ -31,12 +37,24 @@ public class GtrAdapter extends RecyclerView.Adapter<GtrAdapter.GtrViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull GtrViewHolder gtrViewHolder, int i) {
-        Gtr gtr = gtrList.get(i);
+        final Gtr gtr = gtrList.get(i);
 
         gtrViewHolder.textViewYear.setText(gtr.getYear());
         gtrViewHolder.textViewName.setText(gtr.getName());
 
         gtrViewHolder.imageView.setImageDrawable(mCtx.getResources().getDrawable(gtr.getImage()));
+
+        gtrViewHolder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mCtx, gtr.getName(), Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(mCtx, ViewDetail.class);
+                intent.putExtra("image_url", gtr.getImage());
+                intent.putExtra("image_name", gtr.getName());
+                mCtx.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -48,6 +66,7 @@ public class GtrAdapter extends RecyclerView.Adapter<GtrAdapter.GtrViewHolder> {
 
         ImageView imageView;
         TextView textViewName, textViewYear;
+        LinearLayout parentLayout;
 
         public GtrViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -55,6 +74,7 @@ public class GtrAdapter extends RecyclerView.Adapter<GtrAdapter.GtrViewHolder> {
             imageView = itemView.findViewById(R.id.imageView);
             textViewName = itemView.findViewById(R.id.name);
             textViewYear = itemView.findViewById(R.id.year);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
         }
     }
 
---
+++ app/src/main/java/com/example/recyclerview/ViewDetail.java
@@ -0,0 +1,42 @@
+package com.example.recyclerview;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ViewDetail extends AppCompatActivity {
+    private static final String TAG = "ViewDetail";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_gtr);
+        Log.d(TAG, "onCreate: started");
+
+        getIncomingIntent();
+    }
+
+    private void getIncomingIntent(){
+        Log.d(TAG, "getIncomingIntent: checking for incoming intents.");
+
+        if (getIntent().hasExtra("image_url") && getIntent().hasExtra("image_name")){
+            Log.d(TAG, "getIncomingIntent: found intent extras.");
+
+            int imageUrl = getIntent().getExtras().getInt("image_url");
+            String imageName = getIntent().getStringExtra("image_name");
+
+            setImage(imageUrl, imageName);
+        }
+    }
+
+    private void setImage(int imageUrl, String imageName){
+        TextView name = findViewById(R.id.image_name);
+        name.setText(imageName);
+
+        ImageView image = findViewById(R.id.image);
+        image.setImageResource(imageUrl);
+    }
+}
