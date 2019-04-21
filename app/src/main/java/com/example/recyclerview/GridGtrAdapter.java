04d57be64be36d30c152c54da7fa7d8bb77da477
randomize721 <anggadp91@gmail.com>
2019-04-22 00:56:28 +0800

    GridView Enabled, more data

--- app/src/main/java/com/example/recyclerview/GridGtrAdapter.java
+++ app/src/main/java/com/example/recyclerview/GridGtrAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,36 +14,30 @@ import android.widget.Toast;
 
 import java.util.List;
 
-import static android.content.ContentValues.TAG;
-
-public class GtrAdapter extends RecyclerView.Adapter<GtrAdapter.GtrViewHolder> {
+public class GridGtrAdapter extends RecyclerView.Adapter<GridGtrAdapter.GridViewHolder> {
 
     private Context mCtx;
     private List<Gtr> gtrList;
 
-    public GtrAdapter(Context mCtx, List<Gtr> gtrList) {
+    public GridGtrAdapter(Context mCtx, List<Gtr> gtrList) {
         this.mCtx = mCtx;
         this.gtrList = gtrList;
     }
 
     @NonNull
     @Override
-    public GtrViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+    public GridViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         LayoutInflater inflater = LayoutInflater.from(mCtx);
-        View view = inflater.inflate(R.layout.gtr_list, null);
-        return new GtrViewHolder(view);
+        View view = inflater.inflate(R.layout.gtr_grid, null);
+        return new GridViewHolder(view);
     }
-
     @Override
-    public void onBindViewHolder(@NonNull GtrViewHolder gtrViewHolder, int i) {
+    public void onBindViewHolder(@NonNull GridViewHolder gridViewHolder, int i) {
         final Gtr gtr = gtrList.get(i);
 
-        gtrViewHolder.textViewYear.setText(gtr.getYear());
-        gtrViewHolder.textViewName.setText(gtr.getName());
+        gridViewHolder.imageView.setImageDrawable(mCtx.getResources().getDrawable(gtr.getImage()));
 
-        gtrViewHolder.imageView.setImageDrawable(mCtx.getResources().getDrawable(gtr.getImage()));
-
-        gtrViewHolder.parentLayout.setOnClickListener(new View.OnClickListener() {
+        gridViewHolder.parentLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(mCtx, gtr.getName(), Toast.LENGTH_SHORT).show();
@@ -57,27 +50,19 @@ public class GtrAdapter extends RecyclerView.Adapter<GtrAdapter.GtrViewHolder> {
             }
         });
     }
-
     @Override
     public int getItemCount() {
-
         return gtrList.size();
     }
-
-    class GtrViewHolder extends RecyclerView.ViewHolder {
-
+    public class GridViewHolder extends RecyclerView.ViewHolder {
         ImageView imageView;
-        TextView textViewName, textViewYear;
         LinearLayout parentLayout;
 
-        public GtrViewHolder(@NonNull View itemView) {
+        public GridViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            imageView = itemView.findViewById(R.id.imageView);
-            textViewName = itemView.findViewById(R.id.name);
-            textViewYear = itemView.findViewById(R.id.year);
-            parentLayout = itemView.findViewById(R.id.parent_layout);
+            imageView = itemView.findViewById(R.id.imageViewGrid);
+            parentLayout = itemView.findViewById(R.id.parent_layout_grid);
         }
     }
-
 }
--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     GtrAdapter adapter;
+    GridGtrAdapter adapterGrid;
 
     List<Gtr> gtrList;
 
@@ -24,31 +28,90 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        showList();
+    }
+
+    private void items(){
+        gtrList.clear();
+        gtrList.add(
+                new Gtr(
+                        "PGC10/KPGC10 Skyline GT-R",
+                        "1969–1972",
+                        "The first Skyline GT-R, known by the internal Nissan designation PGC10, was released on 4 February 1969, and was exclusive to Japanese Nissan dealership network called Nissan Prince Store when the Prince company was integrated into Nissan operations in 1966.",
+                        R.drawable.d));
+
+        gtrList.add(
+                new Gtr(
+                        "KPGC110 Skyline GT-R",
+                        "1973",
+                        "The KPGC10's successor, the KPGC110, was released in 1973 after its introduction at the 1972 Tokyo Motor Show. Powered by a 1989 cc I6 S20 engine, the second generation GT-R delivered power to the rear wheels through a 5-speed manual gearbox.",
+                        R.drawable.e));
 
         gtrList.add(
                 new Gtr(
                         "Nissan Skyline GTR R32",
-                        "1989",
+                        "1989–1994",
                         "The R32 Skyline debuted in 1989. It was available as either a 2-door coupe or 4-door hardtop sedan.",
                         R.drawable.a));
 
         gtrList.add(
                 new Gtr(
+                        "Nissan Skyline GTR R33",
+                        "1995–1998",
+                        "The E-BCNR33 (R33) was developed in 1995 as a successor to the R32 model. The engine in the R33 was nearly identical to the R32. It used the same turbochargers and the same specification for the manual gearbox, although the syncros were stronger.",
+                        R.drawable.f));
+
+        gtrList.add(
+                new Gtr(
                         "Nissan Skyline GTR R34",
-                        "1998",
+                        "1999–2002",
                         "The GT-R reappeared in 1999, with a revised chassis and other updates.",
                         R.drawable.b));
 
         gtrList.add(
                 new Gtr(
                         "Nissan GTR R35",
-                        "2007",
+                        "2007-present",
                         "The Nissan GT-R is a 2-door 2+2 high performance vehicle produced by Nissan, unveiled in 2007. It is the successor to the Nissan Skyline GT-R, although no longer part of the Skyline range itself, that name now being used for Nissan's luxury-sport market.",
                         R.drawable.c));
 
+        gtrList.add(
+                new Gtr(
+                        "NISSAN CONCEPT 2020 Vision Gran Turismo.",
+                        "N/A",
+                        "N/A",
+                        R.drawable.g));
+    }
+
+    private void showList(){
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        items();
         adapter = new GtrAdapter(this, gtrList);
         recyclerView.setAdapter(adapter);
+    }
 
+    private void showGrid(){
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        items();
+        adapterGrid = new GridGtrAdapter(this, gtrList);
+        recyclerView.setAdapter(adapterGrid);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_list:
+                showList();
+                break;
+            case R.id.action_grid:
+                showGrid();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
